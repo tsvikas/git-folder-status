12b9eb09394310d11467171881bdd0d871c813a0
Tsvika Shapira <tsvikas@gmail.com>
2025-11-29 15:42:56 +0200

    fix mypy issues

--- scripts/sample_output.py
+++ scripts/sample_output.py
@@ -1,24 +1,27 @@
 """Generate sample output."""
 
 from git_folder_status.format import format_report
+from git_folder_status.git_folder_status import RepoStats
 
 report = {
-    "my-repo": {"is_dirty": True},
-    "my-other-repo": {
-        "branches_out_of_sync": {
-            "main": {
-                "remote_branch": "origin/main",
-                "commits_behind": 0,
-                "commits_ahead": 1,
-            },
-            "develop": {
-                "remote_branch": "origin/develop",
-                "commits_behind": 1,
-                "commits_ahead": 3,
-            },
+    "my-repo": RepoStats({"is_dirty": True}),
+    "my-other-repo": RepoStats(
+        {
+            "branches_out_of_sync": {
+                "main": {
+                    "remote_branch": "origin/main",
+                    "commits_behind": 0,
+                    "commits_ahead": 1,
+                },
+                "develop": {
+                    "remote_branch": "origin/develop",
+                    "commits_behind": 1,
+                    "commits_ahead": 3,
+                },
+            }
         }
-    },
-    "my-3rd-repo": {"untracked_files": ["important_file.py"]},
-    "repo-4": {"stash_count": 1},
+    ),
+    "my-3rd-repo": RepoStats({"untracked_files": ["important_file.py"]}),
+    "repo-4": RepoStats({"stash_count": 1}),
 }
 print(format_report(report, include_ok=False, fmt="report"))
